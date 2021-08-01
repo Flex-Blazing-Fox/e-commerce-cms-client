8738dc7d1ad6d102d2aa9fb34b8c86c4ca2e1ca5
Arip Budiman <aripbudiman007@gmail.com>
2021-08-01 22:06:08 +0700

    vue-cli config and tailwind css integration

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate quisquam temporibus id excepturi culpa aperiam facere laudantium corporis? Accusamus ullam at eum ratione et consectetur ab labore suscipit iusto quaerat.</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'Home'
+}
+</script>
