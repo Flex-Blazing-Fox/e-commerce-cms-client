83f92af24c62476c04efce18f1d1f08246012e93
ardiansyakh <abdullah.ard9@gmail.com>
2021-08-08 17:49:59 +0700

    add template

--- src/main.js
+++ src/main.js
@@ -1,12 +1,74 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from 'vue'
+import { createWebHistory, createRouter } from 'vue-router'
 
-Vue.config.productionTip = false
+// styles
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import '@/assets/styles/tailwind.css'
+
+// mouting point for the whole app
+
+import App from '@/App.vue'
+
+// layouts
+
+import Admin from '@/layouts/Admin.vue'
+
+// views for Admin layout
+
+import Dashboard from '@/views/admin/Dashboard.vue'
+import AddProduct from '@/views/admin/AddProduct.vue'
+import ListProduct from '@/views/admin/TablesListProduct.vue'
+import Maps from '@/views/admin/Maps.vue'
+
+// views for Auth layout
+
+import Login from '../src/views/Login.vue'
+
+// views without layouts
+
+import Index from '@/views/Index.vue'
+
+// routes
+
+const routes = [
+  {
+    path: '/admin',
+    redirect: '/admin/dashboard',
+    component: Admin,
+    children: [
+      {
+        path: '/admin/dashboard',
+        component: Dashboard
+      },
+      {
+        path: '/admin/add-product',
+        component: AddProduct
+      },
+      {
+        path: '/admin/product-list',
+        component: ListProduct
+      },
+      {
+        path: '/admin/maps',
+        component: Maps
+      }
+    ]
+  },
+  {
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/',
+    component: Index
+  },
+  { path: '/:pathMatch(.*)*', redirect: '/' }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+createApp(App).use(router).mount('#app')
---
+++ src/views/admin/AddProduct.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex flex-wrap">
+    <div class="w-full lg:w-8/12 px-4">
+      <CardAddProduct />
+    </div>
+  </div>
+</template>
+<script>
+import CardAddProduct from '@/components/Cards/CardAddProduct.vue'
+
+export default {
+  components: {
+    CardAddProduct
+  }
+}
+</script>
